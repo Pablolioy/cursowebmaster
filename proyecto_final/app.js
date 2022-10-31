dca5af1bfa8fc2fa072f23315dad5990e23ee382
Pablo Lioy <pablitolioy@hotmail.com>
2022-10-30 21:03:30 -0300

    Method POST, nodemailer y dotenv

---
+++ proyecto_final/app.js
@@ -0,0 +1,80 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+require('dotenv').config();
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var videojuegosRouter = require('./routes/videojuegos');
+var galeriaRouter = require('./routes/galeria');
+var novedadesRouter = require('./routes/novedades');
+var nosotrosRouter = require('./routes/nosotros');
+var contactoRouter = require('./routes/contacto');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//Rutas
+
+app.use('/videojuegos', videojuegosRouter);
+app.use('/galeria', galeriaRouter);
+app.use('/novedades', novedadesRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/contacto', contactoRouter);
+
+//--------
+
+app.get('/videojuegos', function (req, res){
+  res.render('videojuegos');
+})
+
+app.get('/contacto', function (req, res){
+  res.render('contacto');
+})
+
+app.get('/galeria', function (req, res){
+  res.render('galeria');
+})
+
+app.get('/nosotros', function (req, res){
+  res.render('nosotros');
+})
+
+app.get('/novedades', function (req, res){
+  res.render('novedades');
+})
+
+//-------------
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ proyecto_final/routes/contacto.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('contacto', {
+    isContacto: true
+  });
+});
+
+router.post('/', async (req, res, next) => {
+  console.log(req.body);
+
+  var nombre = req.body.nombre;
+  var mail = req.body.mail;
+  var mensaje = req.body.mensaje;
+
+  var obj = {
+    to: 'pablitolioy@gmail.com',
+    subject: 'Team Cherry',
+    html: nombre + " se contacto desde la web Team Cherry y solicita mas informacion a este correo: " + mail + "<br> Ademas, hiso el siguiente comentario: " + mensaje,
+  }
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    }
+  });
+  
+  var info = await transport.sendMail(obj);
+  
+  res.render('contacto', {
+    isContacto: true,
+    message: "Correo enviado exitosamente",
+  })
+})
+
+
+
+module.exports = router;
