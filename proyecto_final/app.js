5895589792a4cd5ae9b91e2a304e4a506ab8115b
Pablo Lioy <pablitolioy@hotmail.com>
2022-12-06 23:58:00 -0300

    Implentacion de admin/login

--- proyecto_final/app.js
+++ proyecto_final/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var session = require('express-session')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +15,9 @@ var galeriaRouter = require('./routes/galeria');
 var novedadesRouter = require('./routes/novedades');
 var nosotrosRouter = require('./routes/nosotros');
 var contactoRouter = require('./routes/contacto');
+
+var adminLogin = require('./routes/admin/login');
+var adminNoverades = require('./routes/admin/novedades');
 
 var app = express();
 
@@ -26,6 +31,25 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'lvaANDgjhenbvA',
+  resave: false,
+  saveUninitialized: true
+}))
+
+secured = async (req, res, next) => {
+  try{
+    console.log(req.session.id_usuario);
+    if(req.session.id_usuario){
+      next();
+    } else {
+      res.redirect('/admin/login');
+    }
+  } catch(error) {
+    console.log(error);
+  }
+}
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -38,36 +62,17 @@ app.use('/nosotros', nosotrosRouter);
 app.use('/contacto', contactoRouter);
 
 //--------
-
-// app.get('/videojuegos', function (req, res){
-//   res.render('videojuegos');
-// })
-
-// app.get('/contacto', function (req, res){
-//   res.render('contacto');
-// })
-
-// app.get('/galeria', function (req, res){
-//   res.render('galeria');
-// })
-
-// app.get('/nosotros', function (req, res){
-//   res.render('nosotros');
-// })
-
-// app.get('/novedades', function (req, res){
-//   res.render('novedades');
-// })
-
+app.use('/admin/login', adminLogin);
+app.use('/admin/novedades', secured, adminNoverades);
 //-------------
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
