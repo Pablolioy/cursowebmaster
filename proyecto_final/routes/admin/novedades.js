e8b81701d7f126ab2f9409280b89f945b8912965
Pablo Lioy <pablitolioy@hotmail.com>
2022-12-15 23:10:43 -0300

    CRUD, listar y elimitan en novedades

--- proyecto_final/routes/admin/novedades.js
+++ proyecto_final/routes/admin/novedades.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require("../../models/novedadesModel");
+const date = require('date-and-time')
 
-router.get('/', function (req, res, next) {
+
+
+router.get('/', async function (req, res, next) {
+    var novedades = await novedadesModel.getNovedades();
+    // novedades[0].hora_fecha=date.format(novedades[0].hora_fecha,"DD/MM/YYYY HH:MM")
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        usuario: req.session.nombre
+        usuario: req.session.nombre,
+        novedades,
     })
+    console.log(novedades)
+    // console.log(novedades[0].hora_fecha)
 })
 
-module.exports=router;
+router.get('/eliminar/:id', async(req, res, next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades')
+  });
+
+module.exports = router;
